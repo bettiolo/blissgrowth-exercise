05205a1bf2208887ea18339e61adfc380e8451d1
Marco Bettiolo <marco@bettiolo.it>
2023-01-18 12:35:27 +0100

    Added Xata client + NestJS service wrapper

---
+++ libs/xata.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common'
+import { getXataClient, ConnectionsRecord } from './xata'
+import { CreateConnectionDto } from './create-connection.dto'
+
+@Injectable()
+export class XataService {
+  async createConnection(createConnectionDto: CreateConnectionDto): Promise<ConnectionsRecord> {
+    return await getXataClient().db.connections.create(createConnectionDto)
+  }
+}
